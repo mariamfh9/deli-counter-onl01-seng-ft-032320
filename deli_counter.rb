47a2d1f0846e14f2b4bf901ccdc23918d8eb9a93
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-25 02:31:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -29,14 +29,14 @@ end
 line_simple(katz_deli)  
 
 def take_a_number(line, new_person)
-  line.push(new_person) # could say: "line << new_person"
+  line.push(new_person) 
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
 
-take_a_number(katz_deli, "Fyvish")
+take_a_number(katz_deli, "Matz")
 
 def now_serving(line)
-  if line.length == 0 # could say: "if deli.empty?"
+  if line.length == 0 
     puts"There is nobody waiting to be served!"
   else
     puts "Currently serving #{line[0]}." 
