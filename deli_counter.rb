9191d4050df4870ca8b78346b846e45446f0f21a
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-25 02:19:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,14 @@
 # Write your code here.
-def line
-  katz_deli = []
-  i = 0
- while i < katz_deli.length - 1
-    if(katz_deli[i] == nil)
-      puts "The line is currently empty."
-    else 
-      puts 
-    
-end 
+def line(array)
+  if array.length >= 1
+    newarray = []
+    counter = 1 
+    array.each do |name|
+      newarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{newarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
