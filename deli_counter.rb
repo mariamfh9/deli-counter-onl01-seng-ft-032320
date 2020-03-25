1f68aa5597ed83e8fe4a4a1a30c273aed693db29
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-25 02:18:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def line
+  katz_deli = []
+  i = 0
+ while i < katz_deli.length - 1
+    if(katz_deli[i] == nil)
+      puts "The line is currently empty."
+    else 
+      puts 
+    
+end 
