046bbc03170ce4a96b68eac89f9b13c406c2d0db
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-25 02:26:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
-katz_deli = ["Ada", "Grace", "Rivki"]
+katz_deli = ["Ada", "Grace", "Kent"]
 
 
 def line(array) 
@@ -45,8 +45,8 @@ def now_serving(line)
   if line.length == 0 # could say: "if deli.empty?"
     puts"There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}." # could say: "Currently serving #{line.first}."
-    line.shift # this works in the IDE but no on repl.it
+    puts "Currently serving #{line[0]}." 
+    line.shift
   end
 end
 
