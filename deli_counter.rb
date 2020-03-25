351e6524dae6473f95165054e0226e331823ffb3
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-25 02:28:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,12 +17,11 @@ end
 
 line(katz_deli)
 
-def line_simple(array) # this one follows the same mechanics as learn.co
+def line_simple(array) 
   current_line = "The simple line is currently:"
-  array.each.with_index(1) do |value, indexemus|  
-  # "each.with_index" is the method...must use "index"
-    current_line << " #{indexemus}. #{value},"    
-    # "indexemus" is used to illustrate this variable can be different from "index"
+  array.each.with_index(1) do |value, i|  
+    current_line << " #{i}. #{value},"    
+    
   end 
   puts current_line
 end 
